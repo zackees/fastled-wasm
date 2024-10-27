34cc1899fd336929325af13c049974b2d943784c
zackees <z@zackees.com>
2024-10-27 12:56:17 -0700

    feat: add support for live-server in open_browser function

--- src/fastled_wasm/open_browser.py
+++ src/fastled_wasm/open_browser.py
@@ -1,17 +1,44 @@
 import os
+import subprocess
 import webbrowser
 from pathlib import Path
+from shutil import which
+
+PORT = 8081
 
 
-def open_browser(fastled_js: Path) -> None:
-    # Start HTTP server in the fastled_js directory
-    if os.path.exists(fastled_js):
-        print(f"\nStarting HTTP server in {fastled_js}")
-        os.chdir(fastled_js)
+def open_browser_live_server(fastled_js: Path, port: int) -> None:
+    """Start live-server in the fastled_js directory"""
+    print(f"\nStarting live-server in {fastled_js}")
+    print("\nStarting live-server...")
+    try:
+        subprocess.run(
+            ["live-server", f"--port={port}"], check=True, shell=True, cwd=fastled_js
+        )
+    except subprocess.CalledProcessError as err:
+        print(f"Error starting live-server: {err}")
 
-        # Start Python's built-in HTTP server
-        print("\nStarting HTTP server...")
-        webbrowser.open("http://localhost:8000")
-        os.system("python -m http.server")
-    else:
+
+def open_browser_python(fastled_js: Path, port: int) -> None:
+    """Start HTTP server in the fastled_js directory"""
+    print(f"\nStarting HTTP server in {fastled_js}")
+    print("\nStarting Python's built-in HTTP server...")
+    webbrowser.open(f"http://localhost:{port}")
+    subprocess.run(
+        ["python", "-m", "http.server", f"{port}"],
+        check=True,
+        shell=True,
+        cwd=fastled_js,
+    )
+
+
+def open_browser(fastled_js: Path, port: int | None = None) -> None:
+    """Start HTTP server in the fastled_js directory"""
+    if not os.path.exists(fastled_js):
         raise FileNotFoundError(f"Output directory {fastled_js} not found")
+    port = port if port is not None else PORT
+    # Check if live-server is available
+    if which("live-server"):
+        open_browser_live_server(fastled_js, port)
+    else:
+        open_browser_python(fastled_js, port)
