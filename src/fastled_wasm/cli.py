e79d48e0156875760fedfbe8c0a01d629943a8d3
zackees <z@zackees.com>
2024-10-26 20:29:22 -0700

    chore: update linting and testing commands to use 'uv run'

--- src/fastled_wasm/cli.py
+++ src/fastled_wasm/cli.py
@@ -4,10 +4,12 @@ Main entry point.
 
 import sys
 
+# from fastled_wasm.app import main as app_main
+
 
 def main() -> int:
     """Main entry point for the template_python_cmd package."""
-    print("Replace with a CLI entry point.")
+    # return app_main()
     return 0
 
 
