f0345a50726fc0fa9b6b0db7c9727fb2bbba53f8
zackees <z@zackees.com>
2024-11-12 15:40:50 -0800

    test: add platform check for test execution in CI environment

--- tests/test_compile_server.py
+++ tests/test_compile_server.py
@@ -1,3 +1,5 @@
+import os
+import platform
 import unittest
 from pathlib import Path
 
@@ -8,9 +10,19 @@ HERE = Path(__file__).parent
 TEST_DIR = HERE / "test_ino" / "wasm"
 
 
+def _enabled() -> bool:
+    """Check if this system can run the tests."""
+    is_github_runner = "GITHUB_ACTIONS" in os.environ
+    if not is_github_runner:
+        return True
+    # this only works in ubuntu at the moment
+    return platform.system() == "Linux"
+
+
 class WebCompilerTester(unittest.TestCase):
     """Main tester class."""
 
+    @unittest.skipUnless(_enabled(), "Skipping test on non-Linux system on github")
     def test_server(self) -> None:
         """Test basic server start/stop functionality."""
         server = CompileServer(container_name="fastled-wasm-compiler-server-test")
