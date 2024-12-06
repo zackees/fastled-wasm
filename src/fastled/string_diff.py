a37425fe5a343216a8f2391f2cb1ef343b0b9573
zackees <z@zackees.com>
2024-12-06 00:13:43 -0800

    improve string comparisons for fast loading

--- src/fastled/string_diff.py
+++ src/fastled/string_diff.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from rapidfuzz.distance import Levenshtein
+from rapidfuzz import fuzz
 
 
 # Returns the min distance strings. If there is a tie, it returns
@@ -9,18 +9,24 @@ from rapidfuzz.distance import Levenshtein
 def string_diff(
     input_string: str, string_list: list[str], ignore_case=True
 ) -> list[tuple[int, str]]:
-
+    
     def normalize(s: str) -> str:
         return s.lower() if ignore_case else s
-
-    distances = [
-        Levenshtein.distance(normalize(input_string), normalize(s)) for s in string_list
-    ]
+    # distances = [
+    #     #Levenshtein.distance(normalize(input_string), normalize(s)) for s in string_list
+    #     fuzz.partial_ratio(normalize(input_string), normalize(s)) for s in string_list
+    # ]
+    distances: list[int] = []
+    for s in string_list:
+        # dist = Levenshtein.distance(normalize(input_string), normalize(s))
+        dist = fuzz.partial_ratio(normalize(input_string), normalize(s))
+        distances.append(1.0/(dist + 1.0))
     min_distance = min(distances)
     out: list[tuple[int, str]] = []
     for i, d in enumerate(distances):
         if d == min_distance:
             out.append((i, string_list[i]))
+
     return out
 
 
@@ -28,7 +34,7 @@ def string_diff_paths(
     input_string: str | Path, path_list: list[Path], ignore_case=True
 ) -> list[tuple[int, Path]]:
     string_list = [str(p) for p in path_list]
-    tmp = string_diff(str(input_string), string_list, ignore_case)
+    tmp = string_diff(input_string, string_list, ignore_case)
     # out: list[tuple[int, Path]] = [(i, Path(path_list[j])) for i, j in tmp]
     out: list[tuple[int, Path]] = []
     for i, j in tmp:
