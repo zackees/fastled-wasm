5a118c13726284a15180e7ce8e74d1132712cb50
zackees <z@zackees.com>
2024-12-09 15:00:48 -0800

    test: skip test on GitHub Actions

--- tests/test_docker_linux_on_windows.py
+++ tests/test_docker_linux_on_windows.py
@@ -1,11 +1,15 @@
+import os
 import unittest
 
 from fastled.docker_manager import DockerManager
+
+IS_GITHUB = "GITHUB_ACTIONS" in os.environ
 
 
 class ProjectInitTester(unittest.TestCase):
     """Main tester class."""
 
+    @unittest.skipIf(IS_GITHUB, "Skipping test on github")
     def test_compile(self) -> None:
         """Test web compilation functionality with real server."""
         # Test the web_compile function with actual server call
