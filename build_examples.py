44e3c2eab0614236a733f036c75bdb832e52e14c
zackees <z@zackees.com>
2024-12-14 17:34:28 -0800

    feat: implement example building functionality in build_examples.py

--- build_examples.py
+++ build_examples.py
@@ -1,15 +1,45 @@
 
 
 import os
+from shutil import which, copytree, rmtree
+from pathlib import Path
+import subprocess
 
-import shutil
+HERE = Path(__file__).parent.resolve()
+DOCS = HERE / "docs"
 
-if not shutil.which("fastled"):
-    raise FileNotFoundError("fastled executable not found")
+EXAMPLES = [
+    "wasm",
+    "Chromancer",
+    "SdCard",
+]
+
+def _exec(cmd: str) -> None:
+    subprocess.run(cmd, shell=True, check=True)
+
+def build_example(example: str) -> None:
+    src_dir = DOCS / example / "src"
+    _exec(f"fastled --init={example} {src_dir}")
+    assert src_dir.exists()
+    _exec(f"fastled {src_dir} --just-compile")
+    fastled_dir = src_dir / "fastled"
+    assert fastled_dir.exists()
+    # now copy it to the example dir
+    example_dir = HERE / example
+    copytree(fastled_dir, example_dir, dirs_exist_ok=True)
+    # now remove the src dir
+    rmtree(src_dir, ignore_errors=True)
+    print(f"Built {example} example in {example_dir}")
+    assert (example_dir / "fastled.wasm").exists()
 
 
-def test_examples():
-    os.system("fastled test-examples")
-    assert os.path.exists("examples")
+def main() -> int:
+    if not which("fastled"):
+        raise FileNotFoundError("fastled executable not found")
+    for example in EXAMPLES:
+        build_example(example)
+    return 0
 
 
+if __name__ == "__main__":
+    main()
