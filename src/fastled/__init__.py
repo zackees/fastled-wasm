0d5d90d6b0aae6cb426be26dd9230ff0a695bd28
zackees <z@zackees.com>
2024-12-14 19:35:08 -0800

    chore: bump version to 1.1.67

--- src/fastled/__init__.py
+++ src/fastled/__init__.py
@@ -9,7 +9,7 @@ from .compile_server import CompileServer
 from .live_client import LiveClient
 from .types import BuildMode, CompileResult, CompileServerError
 
-__version__ = "1.1.66"
+__version__ = "1.1.67"
 
 
 class Api:
