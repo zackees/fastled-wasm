6edf5541a91d562cb0e7530cf7dfa6de932d1428
zackees <z@zackees.com>
2024-12-16 11:57:52 -0800

    test: skip tests on non-Linux systems in CI

--- tests/test_build_examples.py
+++ tests/test_build_examples.py
@@ -7,9 +7,17 @@ import unittest
 from fastled import Api, Test  # type: ignore
 
 
+def _enabled() -> bool:
+    """Check if this system can run the tests."""
+    from fastled import Test
+
+    return Test.can_run_local_docker_tests()
+
+
 class ApiTester(unittest.TestCase):
     """Main tester class."""
 
+    @unittest.skipUnless(_enabled(), "Skipping test on non-Linux system on github")
     def test_build_all_examples(self) -> None:
         """Test command line interface (CLI)."""
 
