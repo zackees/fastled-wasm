d4878763b389294580e5f8ad265af2ab7e19ec0f
zackees <z@zackees.com>
2025-02-04 19:44:52 -0800

    chore: bump version to 1.2.38

--- src/fastled/__init__.py
+++ src/fastled/__init__.py
@@ -16,7 +16,7 @@ from .types import BuildMode, CompileResult, CompileServerError
 # IMPORTANT! There's a bug in github which will REJECT any version update
 # that has any other change in the repo. Please bump the version as the
 # ONLY change in a commit, or else the pypi update and the release will fail.
-__version__ = "1.2.36"
+__version__ = "1.2.38"
 
 
 class Api:
