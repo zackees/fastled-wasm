fc3111a403fdf8008d0397480aa023e0079c6080
zackees <z@zackees.com>
2025-05-09 21:16:21 -0700

    Bump version to 1.2.66 in __init__.py

--- src/fastled/__init__.py
+++ src/fastled/__init__.py
@@ -13,7 +13,7 @@ from .types import BuildMode, CompileResult, CompileServerError
 # IMPORTANT! There's a bug in github which will REJECT any version update
 # that has any other change in the repo. Please bump the version as the
 # ONLY change in a commit, or else the pypi update and the release will fail.
-__version__ = "1.2.65"
+__version__ = "1.2.66"
 
 DOCKER_FILE = (
     "https://raw.githubusercontent.com/zackees/fastled-wasm/refs/heads/main/Dockerfile"
