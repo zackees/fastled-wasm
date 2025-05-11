461ed716cccd0c3cba71d8713dbf63df1ee52a03
zackees <z@zackees.com>
2025-05-11 06:55:48 -0700

    Add unit test for CLI version comparison

---
+++ tests/unit/test_version.py
@@ -0,0 +1,27 @@
+"""
+Unit test file.
+"""
+
+import subprocess
+import unittest
+
+from fastled import __version__
+
+COMMAND = "fastled --version"
+
+
+class MainTester(unittest.TestCase):
+    """Main tester class."""
+
+    def test_command(self) -> None:
+        """Test command line interface (CLI)."""
+        stdout = subprocess.check_output(COMMAND, shell=True)
+        self.assertEqual(
+            stdout.decode("utf-8").strip(),
+            "1.2.80",
+            f"Version mismatch: {stdout.decode('utf-8').strip()} != {__version__}",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
