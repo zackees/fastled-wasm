9a86c7e23076393cff6ebf7d348720efcda15cc7
zackees <z@zackees.com>
2025-05-14 15:06:04 -0700

    Add __version__ import and move version to a separate file

--- src/fastled/__init__.py
+++ src/fastled/__init__.py
@@ -5,16 +5,12 @@ from multiprocessing import Process
 from pathlib import Path
 from typing import Generator
 
+from .__version__ import __version__
 from .compile_server import CompileServer
 from .live_client import LiveClient
 from .settings import DOCKER_FILE, IMAGE_NAME
 from .site.build import build
 from .types import BuildMode, CompileResult, CompileServerError, FileResponse
-
-# IMPORTANT! There's a bug in github which will REJECT any version update
-# that has any other change in the repo. Please bump the version as the
-# ONLY change in a commit, or else the pypi update and the release will fail.
-__version__ = "1.3.1"
 
 
 class Api:
@@ -224,4 +220,5 @@ __all__ = [
     "BuildMode",
     "FileResponse",
     "DOCKER_FILE",
+    "__version__",
 ]
---
+++ src/fastled/__version__.py
@@ -0,0 +1,4 @@
+# IMPORTANT! There's a bug in github which will REJECT any version update
+# that has any other change in the repo. Please bump the version as the
+# ONLY change in a commit, or else the pypi update and the release will fail.
+__version__ = "1.3.1"
