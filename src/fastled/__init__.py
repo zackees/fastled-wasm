3f7d0b5e41db71617bfd1eedb37d688c7e6421a8
zackees <z@zackees.com>
2025-05-14 00:07:51 -0700

    Bump version to 1.2.93

--- src/fastled/__init__.py
+++ src/fastled/__init__.py
@@ -14,7 +14,7 @@ from .types import BuildMode, CompileResult, CompileServerError, FileResponse
 # IMPORTANT! There's a bug in github which will REJECT any version update
 # that has any other change in the repo. Please bump the version as the
 # ONLY change in a commit, or else the pypi update and the release will fail.
-__version__ = "1.2.92"
+__version__ = "1.2.93"
 
 
 class Api:
