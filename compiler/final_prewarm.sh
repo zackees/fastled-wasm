6ae2cf214cedf616cd2c31026c9cf97a29613bab
zackees <z@zackees.com>
2025-05-18 21:32:46 -0700

    Update final_prewarm script to use uv run for CLI update

--- compiler/final_prewarm.sh
+++ compiler/final_prewarm.sh
@@ -5,22 +5,5 @@ if [ "$RENDER" != "true" ]; then
   exit 0
 fi
 
-git_path=/git/fastled
-fastled_path=/js/fastled
-
-# update the fastled git repo
-cd $git_path
-
-git fetch origin
-git reset --hard origin/master
-#  ["rsync", "-av", "--info=NAME", "--delete", f"{src}/", f"{dst}/"],
-
-cd /js
-
-rsync -av --info=NAME --delete "$git_path/" "$fastled_path/"  --exclude ".git"
-# rsync -av --info=NAME --delete "$git_path/examples" "$fastled_path/src"
-
-# now print out all the files in the current directory
-ls -la
-
+uv run -m fastled_wasm_compiler.cli_update_from_master
 
